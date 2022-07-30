60fc45bc52302c5de8a04673a4e336c9e7c0ceec
matheusinit <matheus.oliveira.s@protonmail.com>
2022-07-30 16:17:04 -0300

    add biblioteca microservice

---
+++ biblioteca_microservice/src/main/java/soap/ServerSoap.java
@@ -0,0 +1,49 @@
+package soap;
+
+import javax.jws.WebService;
+import java.util.Objects;
+
+@WebService(endpointInterface = "soap.Biblioteca")
+public class ServerSoap implements Biblioteca {
+    Livro[] livros = new Livro[1];
+    int size = 0;
+
+    public void adicionarLivro(String name) {
+        System.out.println("Adicionar livro");
+
+        Livro livro = new Livro(name);
+
+        if (livros.length == size) {
+            Livro[] novoLivrosArray = new Livro[size + 1];
+
+            for (int i = 0; i < livros.length; i++) {
+                novoLivrosArray[i] = livros[i];
+            }
+
+            livros = novoLivrosArray;
+        }
+
+
+        livros[size] = livro;
+
+
+        size++;
+    }
+
+    public Livro[] listarLivros() {
+        return livros;
+    }
+
+    public Livro atualizarLivro(String nome, String novoNome) {
+        Livro livro = livros[0];
+
+        for (int i = 0; i < livros.length; i++) {
+            if (livros[i].nome.equals(nome)) {
+                livros[i].nome = novoNome;
+                livro = livros[i];
+            }
+        }
+
+        return livro;
+    }
+}
