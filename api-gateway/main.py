53c5a7ce03e95f87c2cfbc32631a9c6c4ec98b33
matheusinit <matheus.oliveira.s@protonmail.com>
2022-07-30 15:56:39 -0300

    first commit

---
+++ api-gateway/main.py
@@ -0,0 +1,39 @@
+from zeep import Client
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+client = Client('http://127.0.0.1:5000/biblioteca?wsdl')
+
+
+@app.get('/book')
+def get_all_biblioteca_service():
+    list = []
+    livros: list = client.service.listarLivros()
+
+    for livro in livros:
+        list.append({'nome': livro['nome']})
+
+    return jsonify(list)
+
+
+@app.post('/book')
+def adicionar_biblioteca_service():
+    livro_nome = request.json['nome']
+
+    client.service.adicionarLivro(
+        livro_nome
+    )
+
+    return request.data
+
+
+@app.put('/book')
+def atualizar_biblioteca_service():
+    bookToUpdate = request.json['to_update']
+    update = request.json['update']
+
+    livro = client.service.atualizarLivro(bookToUpdate, update)
+
+    return jsonify({'nome': livro})
